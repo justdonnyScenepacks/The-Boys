9dc12a3c663e0e61da45b4fe3ff493857ac247ca
justdonnyScenepacks <donnavanlehman15@gmail.com>
2026-06-25 20:47:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const startBtn = document.getElementById("startBtn");
+const intro = document.getElementById("intro");
 
+startBtn.addEventListener("click", () => {
+  intro.style.display = "none";
+  startGame();
+});
+
+function startGame() {
+  document.body.innerHTML = "<h1 style='text-align:center;margin-top:50vh;'>GAME START!</h1>";
+}
